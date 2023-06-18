61d8ac47b8da00cb3108bed7501877996510c533
mjalizi1376@gmail.com <mjalizi1376@gmail.com>
2023-06-18 21:57:07 +0330

    added  from file

---
+++ src/asaste/From .jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const From  = () => {
+  return (
+    <div>
+        
+
+        <form action="">
+ <h1>Sign in</h1>
+
+
+
+
+        </form>
+
+
+        
+        
+        
+         </div>
+  )
+}
+
+export default From 
