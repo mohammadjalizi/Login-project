bc9f0cc670cd98b2efc80319fc25bbfa2e7b437e
mjalizi1376@gmail.com <mjalizi1376@gmail.com>
2023-06-18 22:02:14 +0330

    create file css or form

--- src/asaste/From .jsx
+++ src/asaste/From .jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-
+import './From.css';
 const From  = () => {
   return (
     <div>
         
 
-        <form action="" className=''>
+        <form action="" className='Fromcard'>
  <h1>Sign in</h1>
 <div>   
 <h3>New User?</h3>  <span>create an account</span>
