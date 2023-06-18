0e9b848cbe68aac2a31993cd8b08244d6125a50b
mjalizi1376@gmail.com <mjalizi1376@gmail.com>
2023-06-18 22:39:15 +0330

    style sheet for input

--- src/asaste/From .jsx
+++ src/asaste/From .jsx
@@ -7,21 +7,23 @@ const From  = () => {
 
         <form action="" className='Fromcard  shadow-2xl rounded-2xl  '>
 
-            <div  className=' ml-7 text-left'>    
+            <div  className='  mr-7 ml-7 text-left'>    
  <h1>Sign in</h1>
 <div className=' flex'>   
 <h3>New User?</h3>  <span className=' text-blue-500' >create an account</span>
 
-
+</div>
 <div>
 
  <label >  username or email  </label>
 
+ <input className=' rounded-xl bg-slate-600 p-2 w-full mt-5 ' type='text'  placeholder=' mjalizi1376@gmail.com '></input>
+
 
 </div>
 
 
-</div>
+
 </div>
 
 
