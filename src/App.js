4b02a79dc098c1c8106d062e98e61205f95fd120
Fsaeidi <129076603+Fsaeidi@users.noreply.github.com>
2023-06-19 10:02:28 +0330

    omit login

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import './App.css';
 import From from './asaste/From ';
-import Login from './components/Login';
+
 
 function App() {
   return (
     <div className="App">
-             <Login/>
+           
 
-
-
-<From/>
+           <From/>
 
     </div>
   );
